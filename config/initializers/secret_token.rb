5d0a0d8acb6ab619016c36e85d278a992856e5c2
Paul Morganthall <paul@morganthall.com>
2013-09-18 16:07:52 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anaf::Application.config.secret_token = '465a7b2190f6167b44bef7b18131dfb6221424cdaf048c607342b631a44036820f084ead3dfac776056bca83f8894541762364b50a98e041fd5fc7d17395417b'
