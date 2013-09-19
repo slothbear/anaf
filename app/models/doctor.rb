e93602c817f49fbb6b7b8b1dd57001f7aa693157
Paul Morganthall <paul@morganthall.com>
2013-09-19 13:58:00 -0400

    Numeric ID is existing specialty, string id is new one.
    Manually find by id (cannot mass-assign).
    Move :id to :name so anaf can save the new specialty
    if everything else succeeds (validations).

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,7 +4,20 @@ class Doctor < ActiveRecord::Base
   attr_accessible :name, :specialty_attributes
 
   def specialty_attributes=(specialty_attrs)
-    self.specialty = Specialty.find_or_initialize_by_id(specialty_attrs.delete(:id))
-    self.specialty.attributes = specialty_attrs
+    s_id = specialty_attrs.delete(:id)
+    if integer?(s_id)
+      self.specialty = Specialty.find(s_id)
+    else
+      self.specialty = Specialty.new(name: s_id)
+    end
   end
+
+  private
+
+  # THANKS to Sarah Mei [1].
+  def integer?(value)
+    value.to_i.to_s == value
+  end
+
+  # [1] http://stackoverflow.com/questions/1235863/test-if-a-string-is-basically-an-integer-in-quotes-using-ruby
 end
