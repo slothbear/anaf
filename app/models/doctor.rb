bbb08b2d149be3b33744772adc8f9c339d928232
Paul Morganthall <paul@morganthall.com>
2013-09-19 13:27:21 -0400

    Manually find existing specialties.
    Mass-assignment of 'id' is not supported.
    # bug: Editing an existing specialty is not recorded.
    # the hidden field added by fields_for(?) overrides the new value.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,9 @@ class Doctor < ActiveRecord::Base
   belongs_to :specialty
   accepts_nested_attributes_for :specialty
   attr_accessible :name, :specialty_attributes
+
+  def specialty_attributes=(specialty_attrs)
+    self.specialty = Specialty.find_or_initialize_by_id(specialty_attrs.delete(:id))
+    self.specialty.attributes = specialty_attrs
+  end
 end
